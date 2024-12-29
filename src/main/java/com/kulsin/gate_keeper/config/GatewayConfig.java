dbf92bcb5b8f71bbd5cf5d927af784d37e796f6c
Kulwant Singh <singh.kulwant@gmx.com>
2024-12-29 22:35:42 +0100

    29DEC2024: added span and trace id

--- src/main/java/com/kulsin/gate_keeper/config/GatewayConfig.java
+++ src/main/java/com/kulsin/gate_keeper/config/GatewayConfig.java
@@ -1,17 +1,24 @@
 package com.kulsin.gate_keeper.config;
 
+import brave.Tracing;
+import brave.propagation.CurrentTraceContext;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.brave.bridge.BraveCurrentTraceContext;
+import io.micrometer.tracing.brave.bridge.BraveTracer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfig {
 
-	/*
-	 * @Bean public RouteLocator customRouteLocator(RouteLocatorBuilder builder) { return
-	 * builder.routes() .route("service1", r -> r.path("/service1/**") .filters(f ->
-	 * f.rewritePath("/service1(?<remaining>/?.*)", "/${remaining}"))
-	 * .uri("http://localhost:8081")) .route("service2", r -> r.path("/service2/**")
-	 * .filters(f -> f.rewritePath("/service2(?<remaining>/?.*)", "/${remaining}"))
-	 * .uri("http://localhost:8082")) .build(); }
-	 */
+	@Bean
+	public Tracer tracer() {
+		Tracing tracing = Tracing.newBuilder().build();
+		brave.Tracer braveTracer = tracing.tracer();
+		CurrentTraceContext currentTraceContext = tracing.currentTraceContext();
+		io.micrometer.tracing.CurrentTraceContext braveCurrentTraceContext = BraveCurrentTraceContext
+			.fromBrave(currentTraceContext);
+		return new BraveTracer(braveTracer, braveCurrentTraceContext);
+	}
 
 }
