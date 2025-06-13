0faf6065edbaba7ac1714ef7e939fef62ac19d66
Kulwant Singh <kulwant.singh@leovegas.com>
2025-06-13 13:48:03 +0200

    Dev3 changes

--- src/main/java/com/kulsin/gate_keeper/logging/LoggingFilter.java
+++ src/main/java/com/kulsin/gate_keeper/logging/LoggingFilter.java
@@ -21,23 +21,33 @@ public class LoggingFilter implements GlobalFilter {
 			Request URI: {}
 			Request Headers: {}
 			Request Body: {}
+			TraceId: {}
+			SpanId: {}
 			--------------------------------------------------------
 			Response Details
 			--------------------------------------------------------
 			Response Status: {}
 			Response Headers: {}
 			Response Body: {}
+			TraceId: {}
+			SpanId: {}
 			--------------------------------------------------------
 			""";
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-		return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-			log.info(LOG_TEMPLATE, exchange.getRequest().getMethod(), exchange.getRequest().getURI(),
-					exchange.getRequest().getHeaders(), exchange.getRequest().getBody(),
-					exchange.getResponse().getStatusCode(), exchange.getResponse().getHeaders(),
-					exchange.getResponse());
-		}));
+		return Mono.deferContextual(contextView -> {
+			String traceId = contextView.getOrDefault("traceId", "");
+			String spanId = contextView.getOrDefault("spanId", "");
+
+			log.info("Request - TraceId: {}, SpanId: {}", traceId, spanId);
+
+			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+				log.info(LOG_TEMPLATE, exchange.getRequest().getMethod(), exchange.getRequest().getURI(),
+						exchange.getRequest().getHeaders(), traceId, spanId, exchange.getResponse().getStatusCode(),
+						exchange.getResponse().getHeaders(), traceId, spanId);
+			}));
+		});
 	}
 
 }
